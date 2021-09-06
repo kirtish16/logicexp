425ea8d3be51855a5a33618e65e64f25441b27de
Kirtish Surangalikar <55601319+kirtish16@users.noreply.github.com>
2021-09-07 00:18:29 +0530

    Initial Commit

---
+++ logicexp/__init__.py
@@ -0,0 +1,7 @@
+from logicexp import Or
+from logicexp import And
+from logicexp import Not
+from logicexp import Xor
+from logicexp import Nand
+from logicexp import Nor
+from logicexp import Calculate
